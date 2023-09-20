b15ba3510d5a8ca66a2995b3b89b5e197428641b
Kondnf <nikitakondratenko246@gmail.com>
2023-09-20 22:22:12 +0300

    HW29

---
+++ main.py
@@ -0,0 +1,11 @@
+# import random
+#
+# someInt = [random.randint(-50, 50) for i in range(100)]
+# print(someInt)
+# with open('number.txt', 'w') as file:
+#     for i in someInt:
+#         file.write(str(i + someInt[-1]) + '\n')
+# with open('number.txt', 'r') as file:
+#     stri = file.readlines()
+#     for i in stri:
+#         print(i, end='')
